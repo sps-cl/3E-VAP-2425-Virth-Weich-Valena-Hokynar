1e660688c4b4b824bff275b741218c3092d7fc0e
ThaudeZ z <jakub.weich@student.sps-cl.cz>
2024-10-31 22:11:46 +0100

    socket.io

---
+++ server.js
@@ -0,0 +1,29 @@
+// server.js
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static("public"));
+
+
+// https://stackoverflow.com/questions/10058226/send-response-to-all-clients-except-sender#answer-10099325
+io.on("connection", (socket) => {
+  console.log("User connected: ${socket.id}");
+
+  socket.on("pohybKameneClient" /* <—— název eventu, který posílá klient */, (data) => {
+    socket.broadcast.emit("pohybKameneServer" /* <—— Název eventu, který posílá server */, data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected: ${socket.id}");
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log("Server listening on port ${PORT}");
+});
